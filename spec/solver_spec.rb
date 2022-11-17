2c2dfe249484274055a3a5e17fb71a27eab81e76
Apple <riyabulia12@gmail.com>
2022-11-17 18:57:18 +0530

    Add tests for reverse string

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,4 +14,23 @@ describe Solver do
       expect(solver.factorial(5)).to eq(120)
     end
   end
+
+  context 'Test to check the reverse method' do
+    it 'should throw NoMethodError when input is non-string type' do
+      expect { solver.reverse_str(1) }.to raise_exception(ArgumentError)
+    end
+
+    it 'should return error for empty string' do
+      expect { solver.reverse_str('') }.to raise_exception(ArgumentError)
+    end
+
+    it 'should reverse the string correctly' do
+      expect(solver.reverse_str('Hello')).to eq 'olleH'
+    end
+
+    it 'should return the string type' do
+      expect(solver.reverse_str('Hello')).to be_kind_of(String)
+    end
+
+  end
 end
