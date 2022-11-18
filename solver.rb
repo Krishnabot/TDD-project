c7f5501dc9d5bff92dcc0790cd9591a90e53d937
Newton <altontonnalumasa@gmail.com>
2022-11-18 11:03:05 +0300

    add solvee code for fizzbuzz

--- solver.rb
+++ solver.rb
@@ -15,4 +15,16 @@ class Solver
     not(str.instance_of?(String)) || (str.empty?) ? (raise ArgumentError, 'Enter only non-empty string') :  str.reverse
 
   end
+
+  def fizz_buzz(m)
+    if (m % 3).zero? && (m % 5).zero?
+      p 'fizzbuzz'
+    elsif (m % 3).zero?
+      p 'fizz'
+    elsif (m % 5).zero?
+      p 'buzz'
+    else 
+      m.to_s
+    end
+  end
 end
